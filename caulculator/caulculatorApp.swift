b3143683adb45bb7e23df802f575e51f1b7c5fc0
jacobfredriksson <jacobfredriksson@live.se>
2023-02-13 15:27:58 +0100

    Initial Commit

---
+++ caulculator/caulculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  caulculatorApp.swift
+//  caulculator
+//
+//  Created by Jacob Fredriksson on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct caulculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
